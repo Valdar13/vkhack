f1302ee6c6a78e3546b3fc9a88e68ad1f6c84d46
vsavinova <vsavinova31@gmail.com>
2017-10-21 15:47:57 +0300

    Add method for post query

--- src/main/java/ru/vkhack/vk_api/Getter.java
+++ src/main/java/ru/vkhack/vk_api/Getter.java
@@ -1,7 +1,6 @@
 package ru.vkhack.vk_api;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -23,6 +22,63 @@ public class Getter {
             System.out.println(response.toString());
         } catch (Exception e){
             e.printStackTrace();
+        }
+    }
+
+    private void printResponse(InputStream is) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(is));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        System.out.println(response.toString());
+    }
+
+    void sendPost(String address, File file){
+        String params = "file=";
+        byte[] data = null;
+        InputStream is = null;
+
+        try {
+            URL url = new URL(address);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            conn.setRequestProperty("Content-Length", "" + Integer.toString(params.getBytes().length));
+            OutputStream os = conn.getOutputStream();
+            BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            String line = null;
+            while ((line = fileReader.readLine()) != null)
+                params = params.concat(line);
+            data = params.getBytes("UTF-8");
+            os.write(data);
+            data = null;
+            conn.connect();
+
+            //Response:
+            int responseCode= conn.getResponseCode();
+
+            printResponse(conn.getInputStream());
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            is = conn.getInputStream();
+//
+//            byte[] buffer = new byte[8192];
+//            int bytesRead;
+//            while ((bytesRead = is.read(buffer)) != -1) {
+//                baos.write(buffer, 0, bytesRead);
+//            }
+//            data = baos.toByteArray();
+        } catch (Exception e) {
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+            } catch (Exception ex) {}
         }
     }
 }
